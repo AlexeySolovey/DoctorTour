e248bffdfe77a29590effd04d29754bd36c4826a
Alexey <Solovey_alexey3@mail.ru>
2016-07-17 16:11:16 +0300

    start

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+
+	<!--
+	<meta name="description" content="Почему тайтлы мешают продвижению вашего сайта. Каким образом слова заключенные в титл  вставляют палки в колеса вашему проекту." />
+	<meta name="keywords" content="мета теги, keywords, description, title" />
+	-->
+
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+
+</head>
+<body>
+	<div class="container">
+		<div class="row">hello</div>
+		<i class="fa fa-vk"></i>
+	</div>
+
+
+
+
+	<script type="text/javascript" src="js/jquery-2.1.3.min.js"></script>
+	<script type="text/javascript" src="js/bootstrap.js"></script>
+	<script type="text/javascript" src="js/common.js"></script>
+</body>
+</html>
