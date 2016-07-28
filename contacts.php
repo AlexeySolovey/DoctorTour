2b1df104bc73330fdc4abec94f9a4ff12b2fb4ab
Gorchak_UA <gorchakua@gmail.com>
2016-07-28 09:46:15 +0300

    contacts

--- contacts.php
+++ contacts.php
@@ -76,15 +76,27 @@
                 </div>
                 <div class="col-md-8">
                     <div class="contacts-map">
-                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2564.53679738485!2d36.241812450520236!3d50.001293179314914!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4127a0e81d17172d%3A0x1555bea8cf1a793e!2z0LLRg9C70LjRhtGPINCf0YPRiNC60ZbQvdGB0YzQutCwLCA1Nywg0KXQsNGA0LrRltCyLCDQpdCw0YDQutGW0LLRgdGM0LrQsCDQvtCx0LvQsNGB0YLRjCwgNjEwMDA!5e0!3m2!1suk!2sua!4v1469602229575" width="100%" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+                        <iframe src="https://www.google.com/maps/d/embed?mid=14lgh6k1yPnvLnp-LVVE3S4-Q9qg" width="100%" height="480"></iframe>
                     </div>
                     <div class="contact-inputs">
                         <form id="loginform">
-                            <input type="text" name="login" placeholder="Ваше имя" class="top-input" />
-                            <input type="text" name="tel" placeholder="Ваш телефон" class="top-input" />
-                            <input type="text" name="email" placeholder="Ваш email" />
-                            <textarea type="text" placeholder="Ваше сообщение"></textarea>
-                            <button type="button" class="btn">ОТПРАВИТЬ СООБЩЕНИЕ</button>
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <input type="text" name="login" placeholder="Ваше имя" class="top-input" />
+                                </div>
+                                <div class="col-md-6">
+                                    <input type="text" name="tel" placeholder="Ваш телефон" class="top-input" />
+                                </div>
+                            </div>
+                           <div class="row">
+                               <div class="col-md-12">
+                                   <input type="text" name="email" placeholder="Ваш email" />
+                                   <textarea type="text" placeholder="Ваше сообщение"></textarea>
+                                   <button type="button" class="btn">ОТПРАВИТЬ СООБЩЕНИЕ</button>
+                               </div>
+                           </div>
+                            
+                            
                         </form>
                     </div>
                 </div>
